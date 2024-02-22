e18375332277e905c8b732e20f35f952833e2239
Quach Minh Tri <triquach543@gmail.com>
2024-02-22 16:31:11 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use("/api/contacts", contactRouter);
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.json({message:"Wellcome to contact book application. "});
-});
-
+app.use((req, res, next) => {
+       return next(new ApiError(404, "Resource not found"));
+    });
+    app.use((err, req, res, next) => {
+        return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+        });
+    });
 module.exports = app;
