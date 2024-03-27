58d14ebd24c18cc4de6c3086ed9ebf9123370f9d
Minh Tri <129945906+tri306@users.noreply.github.com>
2024-03-27 23:46:31 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
-
 const app = express();
 
 // app.use(cors());
-app.use("/api/contacts", contactsRouter);
-// app.use(express.json());
+app.use(express.json());
+app.use("/api/contacts",contactsRouter);
 
-app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
-    });
-app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
+// app.get("/", (req, res)=>{
+//     res.json({message: "Wellcome to contact book application."})
+// });
+
+app.use((req, res, next)=>{
+    return next(new ApiError(404,"Resourse not found"));
+});
+
+app.use((error, req,res, next)=>{
+    return res.status(error.statusCode||500).json({
         message: error.message || "Internal Server Error",
     });
 });
+
 module.exports = app;
